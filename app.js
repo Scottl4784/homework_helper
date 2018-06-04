3d024657d95b00230a9f89c5537d89202fc366f2
Scottl4784 <Scottl4784@gmail.com>
2018-06-04 11:34:21 -0400

    created and connected to database

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index');
 const homeworkController = require('./routes/homeworkController');
 
 const app = express();
+
+// Connect to database
+mongoose.connect('mongodb://localhost/homework_helper')
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch((err) => {
+    console.log('ERROR', err)
+  })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
